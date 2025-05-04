2ca5d3368cb2289ca1d75851e376e12eb5cfd273
meetsojitra08 <meetsojitra314@gmail.com>
2025-05-04 14:19:42 +0530

    Add files via upload

---
+++ django_qr/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class QRCodeForm(forms.Form):
+    restaurant_name = forms.CharField(
+        max_length=50,
+        label="Title ",
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter a name or title for this QR code'
+
+        })
+        )
+    
+    url = forms.URLField(
+        max_length=200,
+        label='URL ',
+        widget=forms.URLInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter the URL you want to encode'
+        })
+        )
