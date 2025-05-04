ea28a529da2c53c2f56e9498f45f934bc7694b97
meetsojitra08 <meetsojitra314@gmail.com>
2025-05-04 15:11:32 +0530

    settings.py

--- django_qr/settings.py
+++ django_qr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)9+6_(3llc53pe&i(_%xg!mhmcmgqe1j7-_c=&v2hiu-zkto8%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
