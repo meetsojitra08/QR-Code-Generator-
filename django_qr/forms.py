90f75a83311f8e84ed2402fabe48eb00aa1a4019
meetsojitra08 <meetsojitra314@gmail.com>
2025-05-05 13:14:24 +0530

    forms.py

--- django_qr/forms.py
+++ django_qr/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class QRCodeForm(forms.Form):
-    restaurant_name = forms.CharField(
+        name = forms.CharField(
         max_length=50,
         label="Title ",
         widget=forms.TextInput(attrs={
